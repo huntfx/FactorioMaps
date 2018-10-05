9b48e20aa569c9f4c64e9e252171067d1dfb7921
L0laapk3 <L0laapk3@gmail.com>
2018-10-05 19:43:11 +0200

    fixed problems with light entity positions with weird offsets

--- autorun.template.lua
+++ autorun.template.lua
@@ -1,11 +1,11 @@
 fm.autorun = {
-    name = "%%NAME%%", --you can specify the image folder name like this if you don't want to use the save name.
-    day = true,
-    night = true,
-    around_build_range = 5.2,
+    name = "%%NAME%%",				-- changing this is currently not supported
+    day = true,			 			-- changing this is currently not supported
+    night = true,		 			-- changing this is currently not supported
+    around_build_range = 5.2,		-- max range from buildings that images will be saved. Feel free to crank this up to very large numbers, it will only render chunks that already exist, it will not generate new ones.
+    around_smaller_range = 1,		-- same as above, but smaller range for the following entity types:
     smaller_types = {"lamp", "electric-pole", "radar", "straight-rail", "curved-rail", "rail-signal", "rail-chain-signal", "locomotive", "cargo-wagon", "fluid-wagon", "car"},
-    around_smaller_range = 1,
     date = "%%DATE%%",
-    mapInfo = %%MAPINFO%%,
-    chunkCache = %%CHUNKCACHE%%
+    mapInfo = %%MAPINFO%%,			-- changing this is not supported
+    chunkCache = %%CHUNKCACHE%%		-- changing this is not supported
 }
