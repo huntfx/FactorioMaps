b04226d26ce5d96f631ef53087b050cdf8e73f2e
L0laapk3 <L0laapk3@gmail.com>
2018-10-30 14:38:12 +0100

    Fixed image offset issue and build range scaling with HD

--- autorun.template.lua
+++ autorun.template.lua
@@ -1,7 +1,7 @@
 fm.autorun = {
     HD = true,
     around_build_range = 5.2,		-- max range from buildings that images will be saved. Feel free to crank this up to very large numbers, it will only render chunks that already exist, it will not generate new ones.
-    around_smaller_range = 1,		-- same as above, but smaller range for the following entity types:
+    around_smaller_range = 1.2,		-- same as above, but smaller range for the following entity types:
     smaller_types = {"lamp", "electric-pole", "radar", "straight-rail", "curved-rail", "rail-signal", "rail-chain-signal", "locomotive", "cargo-wagon", "fluid-wagon", "car"},
     date = "%%DATE%%",
     name = "%%NAME%%",				-- changing this is currently not supported
