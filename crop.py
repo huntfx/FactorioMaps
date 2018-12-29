d34ec7e9b1c68710905c2fed19b4e8024c94a31b
L0laapk3 <L0laapk3@gmail.com>
2018-12-29 17:10:54 +0100

    last commit in python 2

--- crop.py
+++ crop.py
@@ -24,45 +24,56 @@ def work(line, imgsize, folder):
 
 if __name__ == '__main__':
 
-    psutil.Process(os.getpid()).nice(psutil.BELOW_NORMAL_PRIORITY_CLASS or -10)
+	psutil.Process(os.getpid()).nice(psutil.BELOW_NORMAL_PRIORITY_CLASS or -10)
 
-    subname = "\\".join(sys.argv[2:5])
-    toppath = os.path.join((sys.argv[5] if len(sys.argv) > 5 else "..\\..\\script-output\\FactorioMaps"), sys.argv[1])
+	subname = "\\".join(sys.argv[2:5])
+	toppath = os.path.join((sys.argv[5] if len(sys.argv) > 5 else "..\\..\\script-output\\FactorioMaps"), sys.argv[1])
 
-    basepath = os.path.join(toppath, "Images", subname)
-    
+	basepath = os.path.join(toppath, "Images", subname)
+	
 
-    while not os.path.isdir(basepath) or len(os.walk(basepath).next()[1]) == 0:
-        time.sleep(1)
-    folder = os.path.join(basepath, os.walk(basepath).next()[1][0])
-    datapath = os.path.join(basepath, "crop.txt")
-    maxthreads = mp.cpu_count()
+	while not os.path.isdir(basepath) or len(os.walk(basepath).next()[1]) == 0:
+		time.sleep(1)
+	folder = os.path.join(basepath, os.walk(basepath).next()[1][0])
+	datapath = os.path.join(basepath, "crop.txt")
+	maxthreads = mp.cpu_count()
 
 
-    if not os.path.exists(datapath):
-        print("waiting for crop.txt")
-        while not os.path.exists(datapath):
-            time.sleep(1)
+	if not os.path.exists(datapath):
+		print("waiting for crop.txt")
+		while not os.path.exists(datapath):
+			time.sleep(1)
 
-    files = []
-    with open(datapath, "r") as data:
-        imgsize = int(data.readline().rstrip('\n'))
-        for line in data:
-            files.append(line)
-    
-    pool = mp.Pool(processes=maxthreads)
-    
-    while len(files) > 0:
-        print("left: %s" % len(files))
-        files = filter(lambda x: x, pool.map(partial(work, imgsize=imgsize, folder=folder), files, 128))
-        if len(files) > 0:
-            time.sleep(10 if len(files) > 1000 else 1)
+	files = []
+	with open(datapath, "r") as data:
+		imgsize = int(data.readline().rstrip('\n'))
+		for line in data:
+			files.append(line)
+	
+	pool = mp.Pool(processes=maxthreads)
+	
+	progressQueue = mp.Queue()
+	originalSize = len(files)
+	doneSize = 0
+	print("\r{:5.1f}%".format(0))
+	while len(files) > 0:
+		workers = pool.map_async(partial(work, imgsize=imgsize, folder=folder, queue=progressQueue), files, 128)
+		for _ in range(originalSize):
+			if progressQueue.get(True):
+				doneSize += 1
+				progress = float(doneSize) / originalSize
+				#tsiz = tsize()[0]-10
+				#print "\r{:5.1f}% [{}{}]".format(round(progress * 100, 1), "=" * int(progress * tsiz), " " * (tsiz - int(progress * tsiz))),
+		workers.wait()
+		files = filter(lambda x: x, workers.get())
+		if len(files) > 0:
+			time.sleep(10 if len(files) > 1000 else 1)
 
-    
-    waitfilename = os.path.join(basepath, "done.txt")
-    if not os.path.exists(waitfilename):
-        print("waiting for done.txt")
-        while not os.path.exists(waitfilename):
-            time.sleep(1)
+	
+	waitfilename = os.path.join(basepath, "done.txt")
+	if not os.path.exists(waitfilename):
+		print("waiting for done.txt")
+		while not os.path.exists(waitfilename):
+			time.sleep(1)
 
 
