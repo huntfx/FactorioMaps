1d0eb43d93687389dfc66254403c74b7301f3b38
L0laapk3 <L0laapk3@gmail.com>
2019-02-25 20:20:08 +0100

    Download at start to prevent needless crash and corrupting of timeline when theres no internet at the end

--- updateLib.py
+++ updateLib.py
@@ -5,7 +5,6 @@ from urllib.request import urlretrieve, build_opener, install_opener
 from tempfile import gettempdir
 
 
-targetPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "web")
 
 urlList = (
 	"https://cdn.jsdelivr.net/npm/leaflet@1.4.0/dist/leaflet.css",
@@ -23,37 +22,64 @@ urlList = (
 
 
 
-tempPath = os.path.join(gettempdir(), "FactorioMapsTmpLib")
-try:
-	rmtree(tempPath)
-except (FileNotFoundError, NotADirectoryError):
-	pass
 
-os.makedirs(tempPath, exist_ok=True)
+CURRENTVERSION = 1
 
 
-opener = build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0 U GUYS SUCK WHY ARE YOU BLOCKING Python-urllib')]
-install_opener(opener)
-
-for url in urlList:
-	print(f"downloading {url}")
-	urlretrieve(url, os.path.join(tempPath, os.path.basename(urlparse(url).path)))
 
 
+
+
+def update(Force=True):
+
+	targetPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "web")
 	
-try:
-	rmtree(targetPath)
-except (FileNotFoundError, NotADirectoryError):
-	pass
+	if not Force:
+		try:
+			with open(os.path.join(targetPath, "VERSION"), "r") as f:
+				if f.readline() == str(CURRENTVERSION):
+					return False
+		except FileNotFoundError:
+			pass
+
+	tempPath = os.path.join(gettempdir(), "FactorioMapsTmpLib")
+	try:
+		rmtree(tempPath)
+	except (FileNotFoundError, NotADirectoryError):
+		pass
+
+	os.makedirs(tempPath, exist_ok=True)
 
 
-copytree(tempPath, targetPath)
-with open(os.path.join(targetPath, "VERSION"), "w") as f:
-	f.write("1")
+	opener = build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0 U GUYS SUCK WHY ARE YOU BLOCKING Python-urllib')]
+	install_opener(opener)
+
+	for url in urlList:
+		print(f"downloading {url}")
+		urlretrieve(url, os.path.join(tempPath, os.path.basename(urlparse(url).path)))
+
+		
+	try:
+		rmtree(targetPath)
+	except (FileNotFoundError, NotADirectoryError):
+		pass
 
 
-try:
-	rmtree(tempPath)
-except (FileNotFoundError, NotADirectoryError):
-	pass
+	copytree(tempPath, targetPath)
+	with open(os.path.join(targetPath, "VERSION"), "w") as f:
+		f.write(str(CURRENTVERSION))
+
+
+	try:
+		rmtree(tempPath)
+	except (FileNotFoundError, NotADirectoryError):
+		pass
+
+	return True
+
+
+
+		
+if __name__ == '__main__':
+	update(True)
